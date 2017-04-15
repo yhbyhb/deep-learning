c755d3bfba0cbab7f283e7342d0b5da530b70d21
HanByul Yang <hanbyul.yang@gmail.com>
2017-04-15 16:03:51 +0900

    13. Quiz: TensorFlow Softmax

---
+++ 2_convolutional_neural_networks/intro_to_tensorflow/13_quiz_tensorflow_softmax/quiz.py
@@ -0,0 +1,18 @@
+# Solution is available in the other "solution.py" tab
+import tensorflow as tf
+
+
+def run():
+    output = None
+    logit_data = [2.0, 1.0, 0.1]
+    logits = tf.placeholder(tf.float32)
+    
+    # TODO: Calculate the softmax of the logits
+    softmax = tf.nn.softmax(logits)
+    
+    with tf.Session() as sess:
+        # TODO: Feed in the logit data
+        # output = sess.run(softmax,    )
+        output = sess.run(softmax, feed_dict={logits: logit_data})
+
+    return output
