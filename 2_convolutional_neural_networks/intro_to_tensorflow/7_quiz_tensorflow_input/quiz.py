2306ac0291b4af2fff59166133521c96d82f05a5
HanByul Yang <hanbyul.yang@gmail.com>
2017-04-15 15:25:09 +0900

    7. Quiz: Tensorflow Input

---
+++ 2_convolutional_neural_networks/intro_to_tensorflow/7_quiz_tensorflow_input/quiz.py
@@ -0,0 +1,13 @@
+# Solution is available in the other "solution.py" tab
+import tensorflow as tf
+
+
+def run():
+    output = None
+    x = tf.placeholder(tf.int32)
+
+    with tf.Session() as sess:
+        # TODO: Feed the x tensor 123
+        output = sess.run(x, feed_dict={x: 123})
+
+    return output
---
+++ 2_convolutional_neural_networks/intro_to_tensorflow/7_quiz_tensorflow_input/solution.py
@@ -0,0 +1,13 @@
+# Quiz Solution
+# Note: You can't run code in this tab
+import tensorflow as tf
+
+
+def run():
+    output = None
+    x = tf.placeholder(tf.int32)
+
+    with tf.Session() as sess:
+        output = sess.run(x, feed_dict={x: 123})
+
+    return output
